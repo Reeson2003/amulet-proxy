4602ecf10fb5365cc7c408a9a2467b94823d86db
reeson <reeson2003@gmail.com>
2019-02-14 19:45:54 +0300

    initial

---
+++ src/main/java/ru/reeson2003/amuletproxy/AmuletProxyApplication.java
@@ -0,0 +1,14 @@
+package ru.reeson2003.amuletproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmuletProxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmuletProxyApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/ru/reeson2003/amuletproxy/AmuletProxyApplicationTests.java
@@ -0,0 +1,17 @@
+package ru.reeson2003.amuletproxy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AmuletProxyApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
